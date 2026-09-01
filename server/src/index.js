7c00240e4376638b1f8833310984a06d61a175dd
Pedro Henrique <pedro.h.morschbacher@gmail.com>
2026-09-01 14:40:40 -0300

    feat: sistema de registro implementado

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,18 @@
 import express from "express";
-const app = express();
 import routesRegisterSystem from "../src/routes/register.routes.js"
 import errorHandler from "./middlewares/errorHandler.middlerware.js";
+
+const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware de JSON do Express
 app.use(express.json());
 
 // Rota de registro
 app.use(routesRegisterSystem);
 
-// Tratamentos de erros
-app.use(errorHandler);
+// Tratamento Centralizado dos erros!
+app.use(errorHandler)
 app.listen(port, () => {
     console.log(`Rodando API em: http://localhost:${port}`);
 });

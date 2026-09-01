2bdfba8228ccf649d479a88f7b048d1829aca17e
Pedro Henrique <pedro.h.morschbacher@gmail.com>
2026-09-01 13:42:35 -0300

    feat/chore: Nova implementação de caminhos do registro de usuário

--- server/src/routes/register.routes.js
+++ server/src/routes/register.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/register.controller.js";
+import validateRegister from "../middlewares/validateRegister.middleware.js";
 const router = Router();
 
-router.post("/register", registerUser)
+router.post("/register", validateRegister, registerUser);
 
 export default router;

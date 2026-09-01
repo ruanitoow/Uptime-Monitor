4968039efafd9c715ede35398e6c1f88bd88f24f
ruanitoow <ruanitoow@gmail.com>
2026-09-01 14:35:13 -0300

    Fix: Server not being able to run. Prisma fixed.

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
-const app = express();
 import routesRegisterSystem from "../src/routes/register.routes.js"
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routesRegisterSystem);
-
 app.listen(port, () => {
     console.log(`Rodando API em: http://localhost:${port}`);
 });
